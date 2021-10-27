d36bbaad0ddfc62358b73127203cd6ca4662c630
Bhargav <vicharapubhargav462981609@gmail.com>
2021-10-27 21:18:41 +0530

    Stage-01.py file updated

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,19 +1,33 @@
 from src.utils.all_utils import read_yaml, create_directory
 import argparse
-import pandas as pd
 import os
 import shutil
 from tqdm import tqdm
 import logging
 
+log_string = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+logs_dir = "Logs"
+os.makedirs(logs_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(logs_dir,"Running_Logs.log"),level=logging.INFO,format=log_string,filemode='a')
 
 
+def copy_file(source_download_dir,local_data_dir):
+    source_files = os.listdir(source_download_dir)
+    N = len(source_files)
+    for file in tqdm(source_files,total=N,desc= f"Copying File from {source_download_dir} to {local_data_dir}", colour="green"):
+        src = os.path.join(source_download_dir,file)
+        dst = os.path.join(local_data_dir,file)
+        shutil.copy(src, dst)
 
 def get_data(config_path):
     config = read_yaml(config_path)
 
     source_download_dirs = config["source_download_dirs"]
     local_data_dirs = config["local_data_dirs"]
+
+    for source_download_dir,local_data_dir in tqdm(zip(source_download_dirs,local_data_dirs),total=2,desc= "List of Folders", colour="cyan"):
+        create_directory([local_data_dir])
+        copy_file(source_download_dir,local_data_dir)
 
 
 
@@ -24,6 +38,9 @@ if __name__ == '__main__':
 
     parsed_args = args.parse_args()
 
-   
-    get_data(config_path=parsed_args.config)
-       
+    try:
+        logging.info(">>>>>Stage-01 Started...")
+        get_data(config_path=parsed_args.config)
+        logging.info("Stage-01 Completed , Data saved into local Directory <<<<<<\n")
+    except Exception as e:
+        raise e
