31c35aec6dbc95d99e3052e23e44715fe9f46c6e
Bhargav <vicharapubhargav462981609@gmail.com>
2021-10-28 15:55:27 +0530

    utils/create_callbacks file created

---
+++ src/utils/create_callbacks.py
@@ -0,0 +1,11 @@
+
+
+
+def create_and_save_tensorboard_callback(tensorboard_log_dir,callbacks_dir):
+    pass    
+    
+    
+    
+def create_and_save_checkpoint_callback(checkpoint_dir,callbacks_dir):
+    pass
+   
